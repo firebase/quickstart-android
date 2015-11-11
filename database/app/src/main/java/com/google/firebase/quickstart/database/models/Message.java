db6e5b4c4c8947b4697a431e5e528a1a3103d5c1
Ian Barber <ianbarber@google.com>
2015-11-10 16:05:04 -0800

    Initial checkin of database on Android
    Interops with iOS sample
    Still need to fix tests and styling.
    Colors from auth sample
    Uses firebase UI, and updates latest position on new messages
    
    Change-Id: I5972a5c0c71bbf107894e5029b6fca1f69c0ad28

---
+++ database/app/src/main/java/com/google/firebase/quickstart/database/models/Message.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.firebase.quickstart.database.models;
+
+/**
+ * Model class that can be automatically marshaled by the Firebase SDK.
+ */
+public class Message {
+    private String name;
+    private String text;
+
+    /**
+     * Empty constructor needed for Firebase object deserialization.
+     */
+    public Message() {
+    }
+
+    public Message(String name, String text) {
+        this.name = name;
+        this.text = text;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
