e08472224700740c925881a10728ae9ce7e62b90
Arthur Thompson <arthurthompson@google.com>
2016-01-19 09:44:49 -0800

    Add Crash quickstart
    Change-Id: I3e0cd2181f49a698793ab59a0f4bc009877175a0

---
+++ crash/app/src/main/java/com/google/samples/quickstart/crash/MainActivity.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.samples.quickstart.crash;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+
+import com.google.android.gms.crash.Crash;
+
+import java.util.logging.Level;
+
+/**
+ * This Activity shows the different ways of reporting application crashes.
+ * - Report caught crashes with Crash.report().
+ * - Automatically Report uncaught crashes.
+ *
+ * It also shows how to add log messages to crash reports using Crash.log().
+ *
+ * Check https://developers.google.com/firebase/console to view and analyze your crash reports.
+ *
+ * Check https://developers.google.com/firebase/docs/crash/crash-android for more on
+ * Firebase Crash on Android.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Checkbox to indicate when to catch the thrown exception.
+        final CheckBox catchCrashCheckBox = (CheckBox) findViewById(R.id.catchCrashCheckBox);
+
+        // Button that causes the NullPointerException to be thrown.
+        Button crashButton = (Button) findViewById(R.id.crashButton);
+        crashButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Log that crash button was clicked. This version of Crash.log() will include the
+                // message in the crash report as well as show the message in logcat.
+                Crash.log(Level.INFO.intValue(), TAG, "Crash button clicked");
+
+                // If catchCrashCheckBox is checked catch the exception and report is using
+                // Crash.report(). Otherwise throw the exception and let Firebase Crash automatically
+                // report the crash.
+                if (catchCrashCheckBox.isChecked()) {
+                    try {
+                        throw new NullPointerException();
+                    } catch (NullPointerException ex) {
+                        Crash.log(Level.INFO.intValue(), TAG, "NPE caught");
+                        Crash.report(ex);
+                    }
+                } else {
+                    throw new NullPointerException();
+                }
+            }
+        });
+
+        // Log that the Activity was created. This version of Crash.log() will include the message
+        // in the crash report but will not be shown in logcat.
+        Crash.log("Activity created");
+    }
+}
