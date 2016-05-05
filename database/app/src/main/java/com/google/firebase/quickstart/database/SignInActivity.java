c538859b795933dfd697df9e3caa73d73c352f1f
Sam Stern <samstern@google.com>
2016-05-05 12:22:46 -0700

    Move from Anonymous to Email/Password
    Bug: 28586269
    Bug: 28620129
    Change-Id: I9e236e27e43e6a32b415fdb15fe1c3651e398acb

--- database/app/src/main/java/com/google/firebase/quickstart/database/SignInActivity.java
+++ database/app/src/main/java/com/google/firebase/quickstart/database/SignInActivity.java
@@ -3,14 +3,15 @@ package com.google.firebase.quickstart.database;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -18,14 +19,17 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.quickstart.database.models.User;
 
-public class SignInActivity extends BaseActivity {
+public class SignInActivity extends BaseActivity implements View.OnClickListener {
 
     private static final String TAG = "SignInActivity";
 
     private DatabaseReference mDatabase;
     private FirebaseAuth mAuth;
 
+    private EditText mEmailField;
+    private EditText mPasswordField;
     private EditText mUsernameField;
+    private Button mSignInButton;
     private Button mSignUpButton;
 
     @Override
@@ -36,15 +40,16 @@ public class SignInActivity extends BaseActivity {
         mDatabase = FirebaseDatabase.getInstance().getReference();
         mAuth = FirebaseAuth.getInstance();
 
+        // Views
+        mEmailField = (EditText) findViewById(R.id.field_email);
+        mPasswordField = (EditText) findViewById(R.id.field_password);
         mUsernameField = (EditText) findViewById(R.id.field_username);
-        mSignUpButton = (Button) findViewById(R.id.button_sign_in);
+        mSignInButton = (Button) findViewById(R.id.button_sign_in);
+        mSignUpButton = (Button) findViewById(R.id.button_sign_up);
 
-        mSignUpButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                signUp();
-            }
-        });
+        // Click listeners
+        mSignInButton.setOnClickListener(this);
+        mSignUpButton.setOnClickListener(this);
     }
 
     @Override
@@ -57,29 +62,87 @@ public class SignInActivity extends BaseActivity {
         }
     }
 
-    private void signUp() {
+    private void signIn() {
+        Log.d(TAG, "signIn");
+        if (!validateForm(false)) {
+            return;
+        }
+
         showProgressDialog();
-        mAuth.signInAnonymously()
-                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+        String email = mEmailField.getText().toString();
+        String password = mPasswordField.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
-                    public void onSuccess(AuthResult authResult) {
-                        Log.d(TAG, "signInAnonymously:onSuccess:" + authResult);
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signIn:onComplete:" + task.isSuccessful());
                         hideProgressDialog();
 
-                        // Write new user and go to Main Activity
-                        writeNewUser(authResult.getUser(), mUsernameField.getText().toString());
-                        launchMainActivity();
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Throwable throwable) {
-                        Log.w(TAG, "signInAnonymously:onFailure", throwable);
-                        hideProgressDialog();
-
-                        Toast.makeText(SignInActivity.this, "Sign In Failed", Toast.LENGTH_SHORT).show();
+                        if (task.isSuccessful()) {
+                            // Go to Main Activity
+                            launchMainActivity();
+                        } else {
+                            Toast.makeText(SignInActivity.this, "Sign In Failed",
+                                    Toast.LENGTH_SHORT).show();
+                        }
                     }
                 });
+    }
+
+    private void signUp() {
+        Log.d(TAG, "signUp");
+        if (!validateForm(true)) {
+            return;
+        }
+
+        showProgressDialog();
+        String email = mEmailField.getText().toString();
+        String password = mPasswordField.getText().toString();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "createUser:onComplete:" + task.isSuccessful());
+                        hideProgressDialog();
+
+                        if (task.isSuccessful()) {
+                            // Write new user and go to Main Activity
+                            writeNewUser(task.getResult().getUser(), mUsernameField.getText().toString());
+                            launchMainActivity();
+                        } else {
+                            Toast.makeText(SignInActivity.this, "Sign Up Failed",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private boolean validateForm(boolean requireUsername) {
+        boolean result = true;
+        if (TextUtils.isEmpty(mEmailField.getText().toString())) {
+            mEmailField.setError("Required");
+            result = false;
+        } else {
+            mEmailField.setError(null);
+        }
+
+        if (TextUtils.isEmpty(mPasswordField.getText().toString())) {
+            mPasswordField.setError("Required");
+            result = false;
+        } else {
+            mPasswordField.setError(null);
+        }
+
+        if (requireUsername && TextUtils.isEmpty(mUsernameField.getText().toString())) {
+            mUsernameField.setError("Required");
+            result = false;
+        } else {
+            mUsernameField.setError(null);
+        }
+
+        return result;
     }
 
     private void launchMainActivity() {
@@ -93,6 +156,18 @@ public class SignInActivity extends BaseActivity {
         User user = new User(name);
 
         mDatabase.child("users").child(userId).setValue(user);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button_sign_in:
+                signIn();
+                break;
+            case R.id.button_sign_up:
+                signUp();
+                break;
+        }
     }
     // [END basic_write]
 }
