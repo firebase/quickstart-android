c04bef5140d32298fac4b40efeffa8ad184e51df
Arthur Thompson <arthurthompson@google.com>
2016-05-23 11:58:45 -0700

    Handle data accompanying a notification message
    Change-Id: I8cf6320acba15a234b0f091951be4a1a80489f6c

--- messaging/app/src/main/java/com/google/firebase/quickstart/fcm/MainActivity.java
+++ messaging/app/src/main/java/com/google/firebase/quickstart/fcm/MainActivity.java
@@ -34,6 +34,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // If a notification message is tapped, any data accompanying the notification
+        // message is available in the intent extras. In this sample the launcher
+        // intent is fired when the notification is tapped, so any accompanying data would
+        // be handled here. If you want a different intent fired, set the click_action
+        // field of the notification message to the desired intent. The launcher intent
+        // is used when no click_action is specified.
+        //
+        // Handle possible data accompanying notification message.
+        // [START handle_data_extras]
+        if (getIntent().getExtras() != null) {
+            for (String key : getIntent().getExtras().keySet()) {
+                String value = getIntent().getExtras().getString(key);
+                Log.d(TAG, "Key: " + key + " Value: " + value);
+            }
+        }
+        // [END handle_data_extras]
+
         Button subscribeButton = (Button) findViewById(R.id.subscribeButton);
         subscribeButton.setOnClickListener(new View.OnClickListener() {
             @Override
