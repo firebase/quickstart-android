0392b09cf7f54b325b04eaae233cd70147aa3852
Sam Stern <samstern@google.com>
2016-09-14 16:05:08 -0700

    Refactor Storage sample to download in background

--- storage/app/src/main/java/com/google/firebase/quickstart/firebasestorage/MyDownloadService.java
+++ storage/app/src/main/java/com/google/firebase/quickstart/firebasestorage/MyDownloadService.java
@@ -16,7 +16,6 @@
 
 package com.google.firebase.quickstart.firebasestorage;
 
-import android.app.Service;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.IBinder;
@@ -34,28 +33,30 @@ import com.google.firebase.storage.StreamDownloadTask;
 import java.io.IOException;
 import java.io.InputStream;
 
-public class MyDownloadService extends Service {
+/**
+ * Service to handle downloading files from Firebase Storage.
+ */
+public class MyDownloadService extends MyBaseTaskService {
 
     private static final String TAG = "Storage#DownloadService";
 
     /** Actions **/
     public static final String ACTION_DOWNLOAD = "action_download";
-    public static final String ACTION_COMPLETED = "action_completed";
-    public static final String ACTION_ERROR = "action_error";
+    public static final String DOWNLOAD_COMPLETED = "download_completed";
+    public static final String DOWNLOAD_ERROR = "download_error";
 
     /** Extras **/
     public static final String EXTRA_DOWNLOAD_PATH = "extra_download_path";
     public static final String EXTRA_BYTES_DOWNLOADED = "extra_bytes_downloaded";
 
-    private StorageReference mStorage;
-    private int mNumTasks = 0;
+    private StorageReference mStorageRef;
 
     @Override
     public void onCreate() {
         super.onCreate();
 
         // Initialize Storage
-        mStorage = FirebaseStorage.getInstance().getReference();
+        mStorageRef = FirebaseStorage.getInstance().getReference();
     }
 
     @Nullable
@@ -70,81 +71,66 @@ public class MyDownloadService extends Service {
 
         if (ACTION_DOWNLOAD.equals(intent.getAction())) {
             // Get the path to download from the intent
-            final String downloadPath = intent.getStringExtra(EXTRA_DOWNLOAD_PATH);
-
-            // Mark task started
-            Log.d(TAG, ACTION_DOWNLOAD + ":" + downloadPath);
-            taskStarted();
-
-            // Download and get total bytes
-            mStorage.child(downloadPath).getStream(
-                    new StreamDownloadTask.StreamProcessor() {
-                        @Override
-                        public void doInBackground(StreamDownloadTask.TaskSnapshot taskSnapshot,
-                                                   InputStream inputStream) throws IOException {
-                            // Close the stream at the end of the Task
-                            inputStream.close();
-                        }
-                    })
-                    .addOnSuccessListener(new OnSuccessListener<StreamDownloadTask.TaskSnapshot>() {
-                        @Override
-                        public void onSuccess(StreamDownloadTask.TaskSnapshot taskSnapshot) {
-                            Log.d(TAG, "download:SUCCESS");
-
-                            // Send success broadcast with number of bytes downloaded
-                            Intent broadcast = new Intent(ACTION_COMPLETED);
-                            broadcast.putExtra(EXTRA_DOWNLOAD_PATH, downloadPath);
-                            broadcast.putExtra(EXTRA_BYTES_DOWNLOADED, taskSnapshot.getTotalByteCount());
-                            LocalBroadcastManager.getInstance(getApplicationContext())
-                                    .sendBroadcast(broadcast);
-
-                            // Mark task completed
-                            taskCompleted();
-                        }
-                    })
-                    .addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception exception) {
-                            Log.w(TAG, "download:FAILURE", exception);
-
-                            // Send failure broadcast
-                            Intent broadcast = new Intent(ACTION_ERROR);
-                            broadcast.putExtra(EXTRA_DOWNLOAD_PATH, downloadPath);
-                            LocalBroadcastManager.getInstance(getApplicationContext())
-                                    .sendBroadcast(broadcast);
-
-                            // Mark task completed
-                            taskCompleted();
-                        }
-                    });
+            String downloadPath = intent.getStringExtra(EXTRA_DOWNLOAD_PATH);
+            downloadFromPath(downloadPath);
         }
 
         return START_REDELIVER_INTENT;
     }
 
-    private void taskStarted() {
-        changeNumberOfTasks(1);
-    }
+    private void downloadFromPath(final String downloadPath) {
+        Log.d(TAG, "downloadFromPath:" + downloadPath);
 
-    private void taskCompleted() {
-        changeNumberOfTasks(-1);
-    }
+        // Mark task started
+        taskStarted();
 
-    private synchronized void changeNumberOfTasks(int delta) {
-        Log.d(TAG, "changeNumberOfTasks:" + mNumTasks + ":" + delta);
-        mNumTasks += delta;
+        // Download and get total bytes
+        mStorageRef.child(downloadPath).getStream(
+                new StreamDownloadTask.StreamProcessor() {
+                    @Override
+                    public void doInBackground(StreamDownloadTask.TaskSnapshot taskSnapshot,
+                                               InputStream inputStream) throws IOException {
+                        // Close the stream at the end of the Task
+                        inputStream.close();
+                    }
+                })
+                .addOnSuccessListener(new OnSuccessListener<StreamDownloadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(StreamDownloadTask.TaskSnapshot taskSnapshot) {
+                        Log.d(TAG, "download:SUCCESS");
 
-        // If there are no tasks left, stop the service
-        if (mNumTasks <= 0) {
-            Log.d(TAG, "stopping");
-            stopSelf();
-        }
+                        // Send success broadcast with number of bytes downloaded
+                        Intent broadcast = new Intent(DOWNLOAD_COMPLETED);
+                        broadcast.putExtra(EXTRA_DOWNLOAD_PATH, downloadPath);
+                        broadcast.putExtra(EXTRA_BYTES_DOWNLOADED, taskSnapshot.getTotalByteCount());
+                        LocalBroadcastManager.getInstance(getApplicationContext())
+                                .sendBroadcast(broadcast);
+
+                        // Mark task completed
+                        taskCompleted();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        Log.w(TAG, "download:FAILURE", exception);
+
+                        // Send failure broadcast
+                        Intent broadcast = new Intent(DOWNLOAD_ERROR);
+                        broadcast.putExtra(EXTRA_DOWNLOAD_PATH, downloadPath);
+                        LocalBroadcastManager.getInstance(getApplicationContext())
+                                .sendBroadcast(broadcast);
+
+                        // Mark task completed
+                        taskCompleted();
+                    }
+                });
     }
 
     public static IntentFilter getIntentFilter() {
         IntentFilter filter = new IntentFilter();
-        filter.addAction(ACTION_COMPLETED);
-        filter.addAction(ACTION_ERROR);
+        filter.addAction(DOWNLOAD_COMPLETED);
+        filter.addAction(DOWNLOAD_ERROR);
 
         return filter;
     }
