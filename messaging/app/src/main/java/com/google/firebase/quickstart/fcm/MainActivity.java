da03fdd9151011cd8b83d6b4a48408c8fb9952f1
Arthur Thompson <arthurthompson@google.com>
2016-09-15 15:49:12 -0700

    Use Object for extra

--- messaging/app/src/main/java/com/google/firebase/quickstart/fcm/MainActivity.java
+++ messaging/app/src/main/java/com/google/firebase/quickstart/fcm/MainActivity.java
@@ -46,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
         // [START handle_data_extras]
         if (getIntent().getExtras() != null) {
             for (String key : getIntent().getExtras().keySet()) {
-                String value = getIntent().getExtras().getString(key);
+                Object value = getIntent().getExtras().get(key);
                 Log.d(TAG, "Key: " + key + " Value: " + value);
             }
         }
