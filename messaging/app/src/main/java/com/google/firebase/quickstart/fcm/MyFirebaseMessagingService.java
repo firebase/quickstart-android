e96ba817f71a0350654ffbaccf5c7279f8e838af
Hasan Faraaz <hasanandroidapp@gmail.com>
2016-09-25 17:31:23 +0530

    Can display push notification now
    Added line "sendNotification(remoteMessage.getNotification().getBody());" which will now show the notificaiton in the status bar

--- messaging/app/src/main/java/com/google/firebase/quickstart/fcm/MyFirebaseMessagingService.java
+++ messaging/app/src/main/java/com/google/firebase/quickstart/fcm/MyFirebaseMessagingService.java
@@ -63,6 +63,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         if (remoteMessage.getNotification() != null) {
             Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
         }
+        sendNotification(remoteMessage.getNotification().getBody());
 
         // Also if you intend on generating your own notifications as a result of a received FCM
         // message, here is where that should be initiated. See sendNotification method below.
