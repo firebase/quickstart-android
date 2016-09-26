6759f2dc97178c9cc9c327d8579e556873a17e37
Arthur Thompson <arthurthompson@google.com>
2016-09-26 14:21:15 -0700

    Use correct addOnCompleteListener, to avoid leaks.
    In cases where the OnCompleteListener is referring to its containing
    Activity, the addOnCompleteListener(Activity, OnCompleteListener)
    method should be used, to avoid leaks.
    
    bug: 31681848
    Change-Id: I9bca532b0e200ea88c1b5ba422f3d55a1d3dc5a2

--- config/app/src/main/java/com/google/samples/quickstart/config/MainActivity.java
+++ config/app/src/main/java/com/google/samples/quickstart/config/MainActivity.java
@@ -113,7 +113,7 @@ public class MainActivity extends AppCompatActivity {
         // more than cacheExpiration seconds ago. Thus the next fetch would go to the server unless
         // throttling is in progress. The default expiration duration is 43200 (12 hours).
         mFirebaseRemoteConfig.fetch(cacheExpiration)
-                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
                     @Override
                     public void onComplete(@NonNull Task<Void> task) {
                         if (task.isSuccessful()) {
