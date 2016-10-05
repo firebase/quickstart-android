1572468fc7df578618b38cb7a5030d6c1ab48744
Sam Stern <samstern@google.com>
2016-10-05 12:11:38 -0700

    Add FirebaseUI activity to Auth sample (#143)
    Change-Id: I115397b148b0df46af806be95bdca06b8f8b0970

---
+++ auth/app/src/minSdkJellybean/java/com/google/firebase/quickstart/auth/FirebaseUIActivity.java
@@ -0,0 +1,111 @@
+package com.google.firebase.quickstart.auth;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+/**
+ * Demonstrate authentication using the FirebaseUI-Android library. This activity demonstrates
+ * using FirebaseUI for basic email/password sign in.
+ *
+ * For more information, visit https://github.com/firebase/firebaseui-android
+ */
+public class FirebaseUIActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final int RC_SIGN_IN = 9001;
+
+    private FirebaseAuth mAuth;
+
+    private TextView mStatusView;
+    private TextView mDetailView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_firebase_ui);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mStatusView = (TextView) findViewById(R.id.status);
+        mDetailView = (TextView) findViewById(R.id.detail);
+
+        findViewById(R.id.sign_in_button).setOnClickListener(this);
+        findViewById(R.id.sign_out_button).setOnClickListener(this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        updateUI(mAuth.getCurrentUser());
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+                // Sign in succeeded
+                updateUI(mAuth.getCurrentUser());
+            } else {
+                // Sign in failed
+                Toast.makeText(this, "Sign In Failed", Toast.LENGTH_SHORT).show();
+                updateUI(null);
+            }
+        }
+    }
+
+    private void startSignIn() {
+        // Build FirebaseUI sign in intent. For documentation on this operation and all
+        // possible customization see: https://github.com/firebase/firebaseui-android
+        Intent intent = AuthUI.getInstance().createSignInIntentBuilder()
+                .setIsSmartLockEnabled(!BuildConfig.DEBUG)
+                .setProviders(AuthUI.EMAIL_PROVIDER)
+                .setLogo(R.mipmap.ic_launcher)
+                .build();
+
+        startActivityForResult(intent, RC_SIGN_IN);
+    }
+
+    private void updateUI(FirebaseUser user) {
+        if (user != null) {
+            // Signed in
+            mStatusView.setText(getString(R.string.firebaseui_status_fmt, user.getEmail()));
+            mDetailView.setText(getString(R.string.id_fmt, user.getUid()));
+
+            findViewById(R.id.sign_in_button).setVisibility(View.GONE);
+            findViewById(R.id.sign_out_button).setVisibility(View.VISIBLE);
+        } else {
+            // Signed out
+            mStatusView.setText(R.string.signed_out);
+            mDetailView.setText(null);
+
+            findViewById(R.id.sign_in_button).setVisibility(View.VISIBLE);
+            findViewById(R.id.sign_out_button).setVisibility(View.GONE);
+        }
+    }
+
+    private void signOut() {
+        AuthUI.getInstance().signOut(this);
+        updateUI(null);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.sign_in_button:
+                startSignIn();
+                break;
+            case R.id.sign_out_button:
+                signOut();
+                break;
+        }
+    }
+}
