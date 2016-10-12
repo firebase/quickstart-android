cb1b6205504cdf0f2d321da11679a1fbd1c1594c
Sam <samstern@google.com>
2016-10-12 13:31:22 -0700

    Use travis build matrix
    Change-Id: I61206b3bcdb232fcf63c5eb2360300827abd7b4e

--- build.sh
+++ build.sh
@@ -3,43 +3,37 @@
 # Exit on error
 set -e
 
-# List of all samples
-samples=( admob analytics app-indexing auth config crash database dynamiclinks invites messaging storage )
-
 # Limit memory usage
 OPTS='-Dorg.gradle.jvmargs="-Xmx2048m -XX:+HeapDumpOnOutOfMemoryError"'
 
 # Work off travis
-if [[ -v TRAVIS_PULL_REQUEST ]]; then
+if [[ ! -z TRAVIS_PULL_REQUEST ]]; then
   echo "TRAVIS_PULL_REQUEST: $TRAVIS_PULL_REQUEST"
 else
   echo "TRAVIS_PULL_REQUEST: unset, setting to false"
   TRAVIS_PULL_REQUEST=false
 fi
 
-for sample in "${samples[@]}"
-do
-  echo "Building ${sample}"
+echo "Building ${SAMPLE}"
 
-  # Go to sample directory
-  cd $sample
+# Go to sample directory
+cd $SAMPLE
 
-  # Copy mock google-services file if necessary
-  if [ ! -f ./app/google-services.json ]; then
-    echo "Using mock google-services.json"
-    cp ../mock-google-services.json ./app/google-services.json
-  fi
+# Copy mock google-services file if necessary
+if [ ! -f ./app/google-services.json ]; then
+  echo "Using mock google-services.json"
+  cp ../mock-google-services.json ./app/google-services.json
+fi
 
-  # Build
-  if [ $TRAVIS_PULL_REQUEST = false ] ; then
-    # For a merged commit, build all configurations.
-    GRADLE_OPTS=$OPTS ./gradlew clean build
-  else
-    # On a pull request, just build debug which is much faster and catches
-    # obvious errors.
-    GRADLE_OPTS=$OPTS ./gradlew clean :app:assembleDebug
-  fi
+# Build
+if [ $TRAVIS_PULL_REQUEST = false ] ; then
+  # For a merged commit, build all configurations.
+  GRADLE_OPTS=$OPTS ./gradlew clean build
+else
+  # On a pull request, just build debug which is much faster and catches
+  # obvious errors.
+  GRADLE_OPTS=$OPTS ./gradlew clean :app:assembleDebug
+fi
 
-  # Back to parent directory.
-  cd -
-done
+# Back to parent directory.
+cd -
