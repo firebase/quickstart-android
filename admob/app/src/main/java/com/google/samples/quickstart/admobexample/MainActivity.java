58ae55461df93b5edf57711da6e76ea53789883b
Sam Stern <samstern@google.com>
2016-10-13 12:23:24 -0700

    Add error listener to Admob sample

--- admob/app/src/main/java/com/google/samples/quickstart/admobexample/MainActivity.java
+++ admob/app/src/main/java/com/google/samples/quickstart/admobexample/MainActivity.java
@@ -19,6 +19,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.VisibleForTesting;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import com.google.android.gms.ads.AdListener;
@@ -31,8 +32,9 @@ import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.InterstitialAd;
 // [END_EXCLUDE]
 
-
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     private AdView mAdView;
     // [START_EXCLUDE]
@@ -79,6 +81,12 @@ public class MainActivity extends AppCompatActivity {
                 }
                 // [END_EXCLUDE]
             }
+
+            @Override
+            public void onAdFailedToLoad(int i) {
+                // See https://goo.gl/sCZj0H for possible error codes.
+                Log.w(TAG, "onAdFailedToLoad:" + i);
+            }
         });
         // [END create_interstitial_ad_listener]
 
@@ -95,7 +103,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         // [END display_interstitial_ad]
-
 
         // Disable button if an interstitial ad is not loaded yet.
         mLoadInterstitialButton.setEnabled(mInterstitialAd.isLoaded());
