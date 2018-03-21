35a548f9d73edbcdbe88ed2b70092a7e86ece74d
ashwinraghav <ashwinraghav@google.com>
2018-03-21 15:12:59 -0700

    Review feedback

--- functions/app/src/androidTest/java/com/google/samples/quickstart/functions/TestAddNumber.java
+++ functions/app/src/androidTest/java/com/google/samples/quickstart/functions/TestAddNumber.java
@@ -16,11 +16,13 @@ import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
-@LargeTest @RunWith(AndroidJUnit4.class) public class TestAddNumber {
+@LargeTest @RunWith(AndroidJUnit4.class)
+public class TestAddNumber {
   @Rule public ActivityTestRule<MainActivity> mActivityTestRule =
       new ActivityTestRule<>(MainActivity.class);
 
-  @Test public void testAddNumber() {
+  @Test
+  public void testAddNumber() {
     ViewInteraction firstNumber = onView(withId(R.id.field_first_number));
     ViewInteraction secondNumber = onView(withId(R.id.field_second_number));
     ViewInteraction addButton = onView(withId(R.id.button_calculate));
@@ -28,12 +30,14 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
     firstNumber.perform(replaceText("32"));
     secondNumber.perform(replaceText("16"));
+
     addButton.perform(scrollTo(), click());
     // Added a sleep statement to match the app's execution delay.
     // The recommended way to handle such scenarios is to use Espresso idling resources:
     // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+    //TODO(samstern) : add a progress indicator to the UI
     try {
-      Thread.sleep(3000);
+      Thread.sleep(10000);
     } catch (InterruptedException e) {
       e.printStackTrace();
     }
