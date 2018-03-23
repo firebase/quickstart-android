5e1b8ef8bcd70116e30bc4fb873ad18eb9e2bc2f
Ashwin Raghav <ashwinraghav@google.com>
2018-03-23 09:46:48 -0700

    Setup script for environment (#455)

---
+++ functions/setup.sh
@@ -0,0 +1,10 @@
+set -o nounset
+set -e
+
+# Install all of the dependencies of the cloud functions
+pushd functions
+npm install
+popd
+
+# Deploy functions to your Firebase project
+firebase --project="$PROJECT_ID" deploy --only functions
