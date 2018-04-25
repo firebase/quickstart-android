405a03416004de6b30e71687c5df56dfcb524c75
ashwinraghav <ashwinraghav@google.com>
2018-04-25 09:49:01 -0700

    Renaming

--- firestore/app/src/androidTest/java/com/google/firebase/example/fireeats/MainActivityTest.java
+++ firestore/app/src/androidTest/java/com/google/firebase/example/fireeats/MainActivityTest.java
@@ -2,8 +2,6 @@ package com.google.firebase.example.fireeats;
 
 import android.content.Intent;
 import android.support.test.espresso.ViewInteraction;
-import android.support.test.espresso.contrib.RecyclerViewActions;
-import android.support.test.espresso.matcher.BoundedMatcher;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
@@ -11,7 +9,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.example.fireeats.model.Rating;
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
@@ -21,7 +18,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.InstrumentationRegistry.getInstrumentation;
-import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
 import static android.support.test.espresso.action.ViewActions.click;
@@ -30,43 +26,34 @@ import static android.support.test.espresso.action.ViewActions.replaceText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasEntry;
-import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
 
-@LargeTest
-@RunWith(AndroidJUnit4.class)
-public class MainActivityTest {
+@LargeTest @RunWith(AndroidJUnit4.class) public class MainActivityTest {
 
-  @Rule
-  public ActivityTestRule<MainActivity> mActivityTestRule =
+  @Rule public ActivityTestRule<MainActivity> mActivityTestRule =
       new ActivityTestRule<>(MainActivity.class, false, false);
 
-  @Before
-  public void before() {
+  @Before public void before() {
     // Sign out of any existing sessions
     FirebaseAuth.getInstance().signOut();
   }
 
-  @Test
-  public void testAddItemsAndReview() throws InterruptedException {
+  @Test public void testAddItemsAndReview() throws InterruptedException {
     mActivityTestRule.launchActivity(new Intent());
     Thread.sleep(2000);
 
     // Input email for account created in the setup.sh
-    ViewInteraction textInputEditText = onView(withId(R.id.email));
-    textInputEditText.perform(click()).perform(replaceText("test@mailinator.com"));
+    ViewInteraction emailEditText = onView(withId(R.id.email));
+    emailEditText.perform(click()).perform(replaceText("test@mailinator.com"));
     ViewInteraction appCompatButton = onView(withId(R.id.button_next));
     appCompatButton.perform(click());
     Thread.sleep(2000);
 
     //Input password
-    ViewInteraction textInputEditText4 = onView(withId(R.id.password));
-    textInputEditText4.perform(replaceText("password"));
+    ViewInteraction passwordEditText = onView(withId(R.id.password));
+    passwordEditText.perform(replaceText("password"));
     ViewInteraction appCompatButton2 = onView(withId(R.id.button_done));
     appCompatButton2.perform(click());
     Thread.sleep(2000);
@@ -91,14 +78,13 @@ public class MainActivityTest {
     Thread.sleep(2000);
 
     //Write a review
-    ViewInteraction appCompatEditText2 = onView(withId(R.id.restaurant_form_text));
-    appCompatEditText2.perform(replaceText("\uD83D\uDE0E\uD83D\uDE00"), closeSoftKeyboard());
+    ViewInteraction reviewEditText = onView(withId(R.id.restaurant_form_text));
+    reviewEditText.perform(replaceText("\uD83D\uDE0E\uD83D\uDE00"), closeSoftKeyboard());
 
     //Submit the review
-    ViewInteraction appCompatButton3 = onView(withId(R.id.restaurant_form_button));
-    appCompatButton3.perform(click());
+    ViewInteraction submitButton = onView(withId(R.id.restaurant_form_button));
+    submitButton.perform(click());
     Thread.sleep(5000);
-
 
     // Assert that the review exists
     ViewInteraction textView = onView(
@@ -111,14 +97,12 @@ public class MainActivityTest {
       final int position) {
 
     return new TypeSafeMatcher<View>() {
-      @Override
-      public void describeTo(Description description) {
+      @Override public void describeTo(Description description) {
         description.appendText("Child at position " + position + " in parent ");
         parentMatcher.describeTo(description);
       }
 
-      @Override
-      public boolean matchesSafely(View view) {
+      @Override public boolean matchesSafely(View view) {
         ViewParent parent = view.getParent();
         return parent instanceof ViewGroup && parentMatcher.matches(parent) && view.equals(
             ((ViewGroup) parent).getChildAt(position));
