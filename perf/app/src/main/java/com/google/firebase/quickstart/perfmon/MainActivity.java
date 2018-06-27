68a320d32688aea964360f8fd3ede15c9c49dd8a
Sam Stern <samstern@google.com>
2018-06-27 14:48:04 -0700

    Perf sample fixup
    Change-Id: I8403018c6097ca9ce20324560deb7495f9fdf7a0

--- perf/app/src/main/java/com/google/firebase/quickstart/perfmon/MainActivity.java
+++ perf/app/src/main/java/com/google/firebase/quickstart/perfmon/MainActivity.java
@@ -2,9 +2,10 @@ package com.google.firebase.quickstart.perfmon;
 
 import android.graphics.drawable.ColorDrawable;
 import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -16,46 +17,64 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.drawable.GlideDrawable;
 import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.target.Target;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
 import com.google.firebase.perf.FirebasePerformance;
 import com.google.firebase.perf.metrics.Trace;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.Random;
+import java.util.concurrent.Callable;
 import java.util.concurrent.CountDownLatch;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
+
+    private static final String DEFAULT_CONTENT_FILE = "default_content.txt";
+    private static final String CONTENT_FILE = "content.txt";
+    private static final String IMAGE_URL =
+            "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png";
+
     private ImageView mHeader;
     private TextView mContent;
     private Trace mTrace;
+
     private String STARTUP_TRACE_NAME = "startup_trace";
     private String REQUESTS_COUNTER_NAME = "requests sent";
     private String FILE_SIZE_COUNTER_NAME = "file size";
     private CountDownLatch mNumStartupTasks = new CountDownLatch(2);
-    private final String DEFAULT_CONTENT_FILE = "default_content.txt";
-    private final String CONTENT_FILE = "content.txt";
-    private final String IMAGE_URL =
-            "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mHeader = (ImageView) findViewById(R.id.imageView);
-        mContent = (TextView) findViewById(R.id.textView);
-        Button button = (Button) findViewById(R.id.button);
+        mHeader = findViewById(R.id.icon);
+        mContent = findViewById(R.id.textView);
+        Button button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // write 40 chars of random text to file
                 File contentFile = new File(MainActivity.this.getFilesDir(), CONTENT_FILE);
-                new WriteToFileTask(contentFile.getAbsolutePath()).execute(getRandomString(40) + "\n");
-            }
+
+                writeStringToFile(contentFile.getAbsolutePath(), getRandomString(40) + "\n")
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (!task.isSuccessful()) {
+                                    Log.e(TAG, "Unable to write to file", task.getException());
+                                    return;
+                                }
+
+                                loadFileFromDisk();
+                            }
+                        });
+                }
         });
 
         // Begin tracing app startup tasks.
@@ -65,7 +84,7 @@ public class MainActivity extends AppCompatActivity {
         loadImageFromWeb();
         // Increment the counter of number of requests sent in the trace.
         Log.d(TAG, "Incrementing number of requests counter in trace");
-        mTrace.incrementCounter(REQUESTS_COUNTER_NAME);
+        mTrace.incrementMetric(REQUESTS_COUNTER_NAME, 1);
         loadFileFromDisk();
         // Wait for app startup tasks to complete asynchronously and stop the trace.
         new Thread(new Runnable() {
@@ -113,54 +132,69 @@ public class MainActivity extends AppCompatActivity {
                 }).into(mHeader);
     }
 
-    private void loadFileFromDisk() {
-        new AsyncTask<Void, Void, Boolean>(){
-            private String fileContent;
-
-            @Override
-            protected Boolean doInBackground(Void... params) {
-                File contentFile = new File(MainActivity.this.getFilesDir(), CONTENT_FILE);
-                try {
-                    if (contentFile.createNewFile()) {
-                        // Content file exist did not exist in internal storage and new file was created.
-                        // Copy in the default content.
-                        InputStream is;
-                        is = getAssets().open(DEFAULT_CONTENT_FILE);
-                        int size = is.available();
-                        byte[] buffer = new byte[size];
-                        is.read(buffer);
-                        is.close();
-                        FileOutputStream fos = new FileOutputStream(contentFile);
-                        fos.write(buffer);
+    private Task<Void> writeStringToFile(final String filename, final String content) {
+        return Tasks.call(
+                AsyncTask.THREAD_POOL_EXECUTOR,
+                new Callable<Void>() {
+                    @Override
+                    public Void call() throws Exception {
+                        FileOutputStream fos = new FileOutputStream(filename, true);
+                        fos.write(content.getBytes());
                         fos.close();
+                        return null;
                     }
-                    FileInputStream fis = new FileInputStream(contentFile);
-                    byte[] content = new byte[(int) contentFile.length()];
-                    fis.read(content);
-                    fileContent = new String(content);
-                    return true;
+                });
+    }
 
-                } catch (IOException e) {
-                    return false;
-                }
-            }
+    private Task<String> loadStringFromFile() {
+        return Tasks.call(
+                AsyncTask.THREAD_POOL_EXECUTOR,
+                new Callable<String>() {
+                    @Override
+                    public String call() throws Exception {
+                        File contentFile = new File(getFilesDir(), CONTENT_FILE);
+                        if (contentFile.createNewFile()) {
+                            // Content file exist did not exist in internal storage and new file was created.
+                            // Copy in the default content.
+                            InputStream is;
+                            is = getAssets().open(DEFAULT_CONTENT_FILE);
+                            int size = is.available();
+                            byte[] buffer = new byte[size];
+                            is.read(buffer);
+                            is.close();
+                            FileOutputStream fos = new FileOutputStream(contentFile);
+                            fos.write(buffer);
+                            fos.close();
+                        }
+                        FileInputStream fis = new FileInputStream(contentFile);
+                        byte[] content = new byte[(int) contentFile.length()];
+                        fis.read(content);
+                        return new String(content);
+                    }
+                });
+    }
 
-            @Override
-            protected void onPostExecute(Boolean result) {
-                super.onPostExecute(result);
-                if (!result) {
-                    Log.e(TAG, "Couldn't read text file.");
-                    Toast.makeText(MainActivity.this, getString(R.string.text_read_error),
-                            Toast.LENGTH_LONG).show();
-                    return;
-                }
-                mContent.setText(fileContent);
-                // Increment a counter with the file size that was read.
-                Log.d(TAG, "Incrementing file size counter in trace");
-                mTrace.incrementCounter(FILE_SIZE_COUNTER_NAME, fileContent.getBytes().length);
-                mNumStartupTasks.countDown();
-            }
-        }.execute();
+    private void loadFileFromDisk() {
+        loadStringFromFile()
+                .addOnCompleteListener(new OnCompleteListener<String>() {
+                    @Override
+                    public void onComplete(@NonNull Task<String> task) {
+                        if (!task.isSuccessful()) {
+                            Log.e(TAG, "Couldn't read text file.");
+                            Toast.makeText(
+                                    MainActivity.this, getString(R.string.text_read_error), Toast.LENGTH_LONG)
+                                    .show();
+                            return;
+                        }
+
+                        String fileContent = task.getResult();
+                        mContent.setText(task.getResult());
+                        // Increment a counter with the file size that was read.
+                        Log.d(TAG, "Incrementing file size counter in trace");
+                        mTrace.incrementMetric(FILE_SIZE_COUNTER_NAME, fileContent.getBytes().length);
+                        mNumStartupTasks.countDown();
+                    }
+                });
     }
 
     private String getRandomString(int length) {
@@ -172,31 +206,5 @@ public class MainActivity extends AppCompatActivity {
             sb.append(c);
         }
         return sb.toString();
-    }
-
-    private class WriteToFileTask extends AsyncTask<String, Void, Void> {
-        private String filename;
-
-        WriteToFileTask(String filename) {
-            this.filename = filename;
-        }
-
-        @Override
-        protected Void doInBackground(String... params) {
-            String content = params[0];
-            try (FileOutputStream fos = new FileOutputStream(filename, true)){
-                fos.write(content.getBytes());
-            } catch (IOException e) {
-                Log.e(TAG, "Unable to write to file: " + filename);
-                Log.e(TAG, Log.getStackTraceString(e));
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void aVoid) {
-            super.onPostExecute(aVoid);
-            loadFileFromDisk();
-        }
     }
 }
