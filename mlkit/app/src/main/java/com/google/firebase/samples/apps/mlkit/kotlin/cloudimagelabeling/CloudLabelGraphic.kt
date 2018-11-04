d5e3aae11e0d878ea651412fdaac248efcbf44f0
XinyueZ <dev.xinyue.zhao@gmail.com>
2018-11-04 20:05:43 +0100

    Slight optimize the CloudLabelGraphic.kt

--- mlkit/app/src/main/java/com/google/firebase/samples/apps/mlkit/kotlin/cloudimagelabeling/CloudLabelGraphic.kt
+++ mlkit/app/src/main/java/com/google/firebase/samples/apps/mlkit/kotlin/cloudimagelabeling/CloudLabelGraphic.kt
@@ -8,11 +8,10 @@ import com.google.firebase.samples.apps.mlkit.common.GraphicOverlay
 /** Graphic instance for rendering detected label.  */
 class CloudLabelGraphic(private val overlay: GraphicOverlay, private val labels: List<String>) :
     GraphicOverlay.Graphic(overlay) {
-    private val textPaint: Paint = Paint()
 
-    init {
-        textPaint.color = Color.WHITE
-        textPaint.textSize = 60.0f
+    private val textPaint = Paint().apply {
+        color = Color.WHITE
+        textSize = 60.0f
     }
 
     @Synchronized
