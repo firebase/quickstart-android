aa6f416ad51cb4dd918b420155ac07a1e83dd504
Sam Stern <samstern@google.com>
2019-03-15 17:10:55 -0700

    Fix layouts
    Change-Id: I9a90705617bb34b8c35eefc70e009d8c1f94856c

---
+++ mlkit-smartreply/app/src/main/java/com/google/firebase/samples/apps/mlkit/smartreply/EntryChoiceActivity.kt
@@ -0,0 +1,24 @@
+package com.google.firebase.samples.apps.mlkit.smartreply
+
+import android.content.Intent
+import com.firebase.example.internal.BaseEntryChoiceActivity
+import com.firebase.example.internal.Choice
+
+class EntryChoiceActivity : BaseEntryChoiceActivity() {
+
+    override fun getChoices(): List<Choice> {
+        return listOf(
+                Choice(
+                        "Java",
+                        "Run the Firebase ML Kit Smart Reply quickstart written in Java.",
+                        Intent(this,
+                                com.google.firebase.samples.apps.mlkit.smartreply.MainActivity::class.java)),
+                Choice(
+                        "Kotlin",
+                        "Run the Firebase ML Kit Smart Reply quickstart written in Kotlin.",
+                        Intent(
+                                this,
+                                com.google.firebase.samples.apps.mlkit.smartreply.MainActivity::class.java))
+        )
+    }
+}
