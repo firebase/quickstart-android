09b3db061c66499ff8a48d5a1f3d84acf6f56bed
Rosário Pereira Fernandes <rosariofernandes51@gmail.com>
2020-06-18 22:30:55 +0200

    fix(messaging): fix wrong nullability check on MessagingService (#1134)
    * fix(messaging): fix wrong nullability check on MessagingService
    
    * refactor(messaging): use string template on MessagingService

--- messaging/app/src/main/java/com/google/firebase/quickstart/fcm/kotlin/MyFirebaseMessagingService.kt
+++ messaging/app/src/main/java/com/google/firebase/quickstart/fcm/kotlin/MyFirebaseMessagingService.kt
@@ -39,8 +39,8 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         Log.d(TAG, "From: ${remoteMessage.from}")
 
         // Check if message contains a data payload.
-        remoteMessage.data.isNotEmpty().let {
-            Log.d(TAG, "Message data payload: " + remoteMessage.data)
+        if (remoteMessage.data.isNotEmpty()) {
+            Log.d(TAG, "Message data payload: ${remoteMessage.data}")
 
             if (/* Check if data needs to be processed by long running job */ true) {
                 // For long-running tasks (10 seconds or more) use WorkManager.
