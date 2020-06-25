a9831a7b681b344f4027e80a1c9826fcb09d9b55
Sam <samstern@google.com>
2020-06-25 11:33:42 -0400

    Fix build
    Change-Id: I0246e356ee02bde9e3f9ab97bcd09b8aec29e57e

--- auth/app/src/main/java/com/google/firebase/quickstart/auth/kotlin/GoogleSignInActivity.kt
+++ auth/app/src/main/java/com/google/firebase/quickstart/auth/kotlin/GoogleSignInActivity.kt
@@ -6,7 +6,6 @@ import com.google.android.material.snackbar.Snackbar
 import android.util.Log
 import android.view.View
 import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
