17a96635e647bce5e071c2bccc8a2195d1c2cc11
Rosário Pereira Fernandes <rosariofernandes51@gmail.com>
2020-09-24 15:38:02 +0200

    Update MainActivity.kt

--- analytics/app/src/main/java/com/google/firebase/quickstart/analytics/kotlin/MainActivity.kt
+++ analytics/app/src/main/java/com/google/firebase/quickstart/analytics/kotlin/MainActivity.kt
@@ -212,7 +212,7 @@ class MainActivity : AppCompatActivity() {
      * we change fragments.
      */
     private fun recordScreenView() {
-        // This string must be <= 36 characters long in order for setCurrentScreen to succeed.
+        // This string must be <= 36 characters long.
         val screenName = "${getCurrentImageId()}-${getCurrentImageTitle()}"
 
         // [START set_current_screen]
