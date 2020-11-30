a2226af98311daa0f841365779973fb5fff55067
Rosário Pereira Fernandes <rosariofernandes51@gmail.com>
2020-11-30 18:40:03 +0200

    refactor(database, firestore): remove kotlin synthetic binding (#1220)

--- database/app/src/main/java/com/google/firebase/quickstart/database/kotlin/viewholder/PostViewHolder.kt
+++ database/app/src/main/java/com/google/firebase/quickstart/database/kotlin/viewholder/PostViewHolder.kt
@@ -2,30 +2,32 @@ package com.google.firebase.quickstart.database.kotlin.viewholder
 
 import androidx.recyclerview.widget.RecyclerView
 import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import com.google.firebase.quickstart.database.R
 import com.google.firebase.quickstart.database.kotlin.models.Post
-import kotlinx.android.synthetic.main.include_post_author.view.postAuthor
-import kotlinx.android.synthetic.main.include_post_text.view.postBody
-import kotlinx.android.synthetic.main.include_post_text.view.postTitle
-import kotlinx.android.synthetic.main.item_post.view.postNumStars
-import kotlinx.android.synthetic.main.item_post.view.star
 
 class PostViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    private val postTitle: TextView = itemView.findViewById(R.id.postTitle)
+    private val postAuthor: TextView = itemView.findViewById(R.id.postAuthor)
+    private val postNumStars: TextView = itemView.findViewById(R.id.postNumStars)
+    private val postBody: TextView = itemView.findViewById(R.id.postBody)
+    private val star: ImageView = itemView.findViewById(R.id.star)
 
     fun bindToPost(post: Post, starClickListener: View.OnClickListener) {
-        itemView.postTitle.text = post.title
-        itemView.postAuthor.text = post.author
-        itemView.postNumStars.text = post.starCount.toString()
-        itemView.postBody.text = post.body
+        postTitle.text = post.title
+        postAuthor.text = post.author
+        postNumStars.text = post.starCount.toString()
+        postBody.text = post.body
 
-        itemView.star.setOnClickListener(starClickListener)
+        star.setOnClickListener(starClickListener)
     }
 
     fun setLikedState(liked: Boolean) {
         if (liked) {
-            itemView.star.setImageResource(R.drawable.ic_toggle_star_24)
+            star.setImageResource(R.drawable.ic_toggle_star_24)
         } else {
-            itemView.star.setImageResource(R.drawable.ic_toggle_star_outline_24)
+            star.setImageResource(R.drawable.ic_toggle_star_outline_24)
         }
     }
 }
