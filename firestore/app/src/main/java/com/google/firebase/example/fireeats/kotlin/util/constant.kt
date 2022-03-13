20938f3ff3e4386620f30fb3638f7e5ffbec9947
Silentou <101495450+Silentou@users.noreply.github.com>
2022-03-13 12:40:49 +0530

    Create constant.kt

---
+++ firestore/app/src/main/java/com/google/firebase/example/fireeats/kotlin/util/constant.kt
@@ -0,0 +1,11 @@
+package com.example.megworld.utilis
+
+import com.example.megworld.models.User
+
+object Constant {
+    const val USERS: String = "users"
+    const val  MEGWORLD_PREFERNCE: String ="MegWorldPrefs"
+    const val  LOGGED_IN_USERNAME: String = "Logged_in_username"
+    const val EXTRA_USER_DETAILS: String = "extra_user_details"
+
+}
