2a58626ef5dd0aa170cf9497b7efbbc774a6e511
rosariopf <rosariopf@google.com>
2022-09-15 15:19:55 +0100

    refactor(config): add a jetpack compose activity

--- config/app/src/main/java/com/google/samples/quickstart/config/ComposeActivity.kt
+++ config/app/src/main/java/com/google/samples/quickstart/config/ComposeActivity.kt
@@ -1,6 +1,40 @@
 package com.google.samples.quickstart.config
 
+import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.google.samples.quickstart.config.ui.theme.QuickstartandroidTheme
 
 class ComposeActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            QuickstartandroidTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    QuickstartandroidTheme {
+        Greeting("Android")
+    }
 }
