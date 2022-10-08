a51ca0425784b6a6ca8cbd531894f1a623969ca7
AlgyJr <algymussa@gmail.com>
2022-10-09 00:31:54 +0200

    :fire: fix: add missing import

--- firestore/app/src/main/java/com/google/firebase/example/fireeats/java/RestaurantDetailFragment.java
+++ firestore/app/src/main/java/com/google/firebase/example/fireeats/java/RestaurantDetailFragment.java
@@ -12,6 +12,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.findNavController;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.bumptech.glide.Glide;
