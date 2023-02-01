d1274830c7c9fca4c06d860b8c24a5f41fc2b8c8
millandavid <71908868+millandavid@users.noreply.github.com>
2023-01-31 18:35:46 -0600

    Added right imports
    Added scaffold and fillMaxWidth imports. Currently having issues with the Scaffold and the experimental wanting it to use wildcard statements

--- config/app/src/main/java/com/google/samples/quickstart/config/kotlin/MainComposeActivity.kt
+++ config/app/src/main/java/com/google/samples/quickstart/config/kotlin/MainComposeActivity.kt
@@ -9,10 +9,12 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.TopAppBar
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
