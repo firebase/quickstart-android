011d70e1ad868cf8aade5a4d0fdd823fdef33906
leesin <dragon02229@naver.com>
2023-04-25 12:14:39 +0900

    unified imports

--- admob/app/src/main/java/com/google/samples/quickstart/admobexample/EntryChoiceActivity.kt
+++ admob/app/src/main/java/com/google/samples/quickstart/admobexample/EntryChoiceActivity.kt
@@ -3,7 +3,6 @@ package com.google.samples.quickstart.admobexample
 import android.content.Intent
 import com.firebase.example.internal.BaseEntryChoiceActivity
 import com.firebase.example.internal.Choice
-import com.google.samples.quickstart.admobexample.java.MainActivity
 
 class EntryChoiceActivity : BaseEntryChoiceActivity() {
 
@@ -12,7 +11,7 @@ class EntryChoiceActivity : BaseEntryChoiceActivity() {
                 Choice(
                         "Java",
                         "Run the Firebase Admob quickstart written in Java.",
-                        Intent(this, MainActivity::class.java)),
+                        Intent(this, com.google.samples.quickstart.admobexample.java.MainActivity::class.java)),
                 Choice(
                         "Kotlin",
                         "Run the Firebase Admob quickstart written in Kotlin.",
--- auth/app/src/main/java/com/google/firebase/quickstart/auth/EntryChoiceActivity.kt
+++ auth/app/src/main/java/com/google/firebase/quickstart/auth/EntryChoiceActivity.kt
@@ -3,7 +3,6 @@ package com.google.firebase.quickstart.auth
 import android.content.Intent
 import com.firebase.example.internal.BaseEntryChoiceActivity
 import com.firebase.example.internal.Choice
-import com.google.firebase.quickstart.auth.java.MainActivity
 
 class EntryChoiceActivity : BaseEntryChoiceActivity() {
 
@@ -12,7 +11,7 @@ class EntryChoiceActivity : BaseEntryChoiceActivity() {
                 Choice(
                         "Java",
                         "Run the Firebase Auth quickstart written in Java.",
-                        Intent(this, MainActivity::class.java)),
+                        Intent(this, com.google.firebase.quickstart.auth.java.MainActivity::class.java)),
                 Choice(
                         "Kotlin",
                         "Run the Firebase Auth quickstart written in Kotlin.",
