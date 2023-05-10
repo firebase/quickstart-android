668aa31dac62944b833498f77d801b90eaa44ce6
Marina Coelho <marinacoelho16@gmail.com>
2023-05-10 12:24:58 +0100

    Migrated database module to most recent AGP and KTS syntax

---
+++ database/app/build.gradle.kts
@@ -0,0 +1,81 @@
+import com.android.build.gradle.internal.tasks.factory.dependsOn
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("com.google.gms.google-services")
+}
+
+tasks {
+    check.dependsOn("assembleDebugAndroidTest")
+}
+
+android {
+    namespace = "com.google.firebase.quickstart.database"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.google.firebase.quickstart.database"
+        minSdk = 19
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+        multiDexEnabled = true
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation(project(":internal:lintchecks"))
+    implementation(project(":internal:chooserx"))
+
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.recyclerview:recyclerview:1.3.0")
+    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
+    implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
+
+    // Import the Firebase BoM (see: https://firebase.google.com/docs/android/learn-more#bom)
+    implementation(platform("com.google.firebase:firebase-bom:32.0.0"))
+
+    // Firebase Realtime Database (Java)
+    implementation("com.google.firebase:firebase-database")
+
+    // Firebase Realtime Database (Kotlin)
+    implementation("com.google.firebase:firebase-database-ktx")
+
+    // Firebase Authentication (Java)
+    implementation("com.google.firebase:firebase-auth")
+
+    // Firebase Authentication (Kotlin)
+    implementation("com.google.firebase:firebase-auth-ktx")
+
+    implementation("com.firebaseui:firebase-ui-database:8.0.2")
+
+    // Needed to fix a dependency conflict with FirebaseUI'
+    implementation("androidx.arch.core:core-runtime:2.2.0")
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test:rules:1.5.0")
+    androidTestImplementation("androidx.test:runner:1.5.2")
+}
