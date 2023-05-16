4fc2d24d565bdd0c85340dacadbc311d3fd55ead
Rosário P. Fernandes <rosariopf@google.com>
2023-05-17 00:50:44 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
     id("com.google.firebase.crashlytics") version "2.9.4" apply false
     id("com.google.firebase.firebase-perf") version "1.4.2" apply false
     id("androidx.navigation.safeargs") version "2.5.3" apply false
-    id("com.github.ben-manes.versions") version "0.41.0" apply false
+    id("com.github.ben-manes.versions") version "0.41.0" apply true
 }
 
 allprojects {
