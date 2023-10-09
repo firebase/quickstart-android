f96650c0944f2c85aeaa7b70c0a388d4cde73343
DPEBot <dpebot@google.com>
2023-10-09 16:41:35 -0700

    Auto-update dependencies. (#1532)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,14 @@
 import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 
 plugins {
-    id("com.android.application") version "8.1.1" apply false
-    id("com.android.library") version "8.1.1" apply false
+    id("com.android.application") version "8.1.2" apply false
+    id("com.android.library") version "8.1.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
     id("com.google.gms.google-services") version "4.4.0" apply false
     id("com.google.firebase.crashlytics") version "2.9.9" apply false
     id("com.google.firebase.firebase-perf") version "1.4.2" apply false
-    id("androidx.navigation.safeargs") version "2.7.3" apply false
-    id("com.github.ben-manes.versions") version "0.48.0" apply true
+    id("androidx.navigation.safeargs") version "2.7.4" apply false
+    id("com.github.ben-manes.versions") version "0.49.0" apply true
 }
 
 allprojects {
--- database/app/build.gradle.kts
+++ database/app/build.gradle.kts
@@ -51,8 +51,8 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.recyclerview:recyclerview:1.3.1")
     implementation("com.google.android.material:material:1.9.0")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.3")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.3")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.4")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.4")
 
     // Import the Firebase BoM (see: https://firebase.google.com/docs/android/learn-more#bom)
     implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
