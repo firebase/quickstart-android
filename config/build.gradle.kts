9468946e30fa54d40cfbc37f0d2fa907e28df29f
DPEBot <dpebot@google.com>
2023-11-08 08:03:51 -0800

    Auto-update dependencies. (#1547)

--- config/build.gradle.kts
+++ config/build.gradle.kts
@@ -1,8 +1,8 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 plugins {
-    id("com.android.application") version "8.1.2" apply false
-    id("com.android.library") version "8.1.2" apply false
+    id("com.android.application") version "8.1.3" apply false
+    id("com.android.library") version "8.1.3" apply false
     id("org.jetbrains.kotlin.android") version "1.9.20" apply false
     id("com.google.gms.google-services") version "4.4.0" apply false
 }
