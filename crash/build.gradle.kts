bdb7cad7d5faad4cbee7c27de8d3a5cf4ae7284d
DPEBot <dpebot@google.com>
2023-11-29 15:49:57 -0800

    Auto-update dependencies. (#1549)

--- crash/build.gradle.kts
+++ crash/build.gradle.kts
@@ -3,7 +3,7 @@
 plugins {
     id("com.android.application") version "8.1.4" apply false
     id("com.android.library") version "8.1.4" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.21" apply false
     id("com.google.gms.google-services") version "4.4.0" apply false
     id("com.google.firebase.crashlytics") version "2.9.9" apply false
 }
--- firestore/app/build.gradle.kts
+++ firestore/app/build.gradle.kts
@@ -76,7 +76,7 @@ dependencies {
     implementation("androidx.cardview:cardview:1.0.0")
     implementation("androidx.browser:browser:1.5.0")
     implementation("com.google.android.material:material:1.9.0")
-    implementation("androidx.media:media:1.6.0")
+    implementation("androidx.media:media:1.7.0")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("androidx.multidex:multidex:2.0.1")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
