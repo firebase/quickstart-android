3527cc7aa019bab6ccbe54eb8e691bd6df5acdf0
DPEBot <dpebot@google.com>
2023-12-01 05:11:18 -0800

    Auto-update dependencies. (#1551)
    * Auto-update dependencies.
    
    * chore(*): bump Gradle to 8.2
    
    ---------
    
    Co-authored-by: rosariopf <rosariopf@google.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 
 plugins {
-    id("com.android.application") version "8.1.4" apply false
-    id("com.android.library") version "8.1.4" apply false
+    id("com.android.application") version "8.2.0" apply false
+    id("com.android.library") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.21" apply false
     id("com.google.gms.google-services") version "4.4.0" apply false
     id("com.google.firebase.crashlytics") version "2.9.9" apply false
