770b163c8c0b409d2259b30c70e5bcc2732e6da4
DPEBot <dpebot@google.com>
2024-01-24 06:39:14 -0800

    Auto-update dependencies. (#1582)

--- internal/lint/build.gradle.kts
+++ internal/lint/build.gradle.kts
@@ -9,8 +9,8 @@ java {
 }
 
 dependencies {
-    compileOnly("com.android.tools.lint:lint-api:31.2.1")
-    testImplementation("com.android.tools.lint:lint:31.2.1")
-    testImplementation("com.android.tools.lint:lint-tests:31.2.1")
+    compileOnly("com.android.tools.lint:lint-api:31.2.2")
+    testImplementation("com.android.tools.lint:lint:31.2.2")
+    testImplementation("com.android.tools.lint:lint-tests:31.2.2")
     testImplementation("junit:junit:4.13.2")
 }
