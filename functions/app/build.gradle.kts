f44c1569a0917b4a904caafd73f2490581dad750
DPEBot <dpebot@google.com>
2024-02-15 15:32:39 -0800

    Auto-update dependencies. (#1590)

--- functions/app/build.gradle.kts
+++ functions/app/build.gradle.kts
@@ -66,7 +66,7 @@ dependencies {
     implementation("com.firebaseui:firebase-ui-auth:8.0.2")
     
     // Google Play services
-    implementation("com.google.android.gms:play-services-auth:20.7.0")
+    implementation("com.google.android.gms:play-services-auth:21.0.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
