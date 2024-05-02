4e6c3e7776e3fb354a4475dae65b71efbc156b6a
rosariopf <rosariopf@google.com>
2024-05-02 23:55:38 +0100

    chore(crashlytics): revert gradle plugin to 2.9.9

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     id("com.android.library") version "8.4.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.23" apply false
     id("com.google.gms.google-services") version "4.4.1" apply false
-    id("com.google.firebase.crashlytics") version "3.0.0" apply false
+    id("com.google.firebase.crashlytics") version "2.9.9" apply false
     id("com.google.firebase.firebase-perf") version "1.4.2" apply false
     id("androidx.navigation.safeargs") version "2.7.7" apply false
     id("com.github.ben-manes.versions") version "0.51.0" apply true
