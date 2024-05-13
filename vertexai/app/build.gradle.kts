659a8d4facf15fc924ea7c9d9a606b276f083538
DPEBot <dpebot@google.com>
2024-05-13 16:21:53 -0700

    Auto-update dependencies. (#1619)

--- vertexai/app/build.gradle.kts
+++ vertexai/app/build.gradle.kts
@@ -54,19 +54,19 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.1")
-    implementation("androidx.navigation:navigation-compose:2.7.5")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
+    implementation("androidx.activity:activity-compose:1.9.0")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
 
-    implementation(platform("androidx.compose:compose-bom:2023.10.01"))
+    implementation(platform("androidx.compose:compose-bom:2024.05.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-
-    implementation("io.coil-kt:coil-compose:2.5.0")
-    implementation("com.google.firebase:firebase-analytics:21.6.1")
+    implementation("io.coil-kt:coil-compose:2.6.0")
+    
+    implementation("com.google.firebase:firebase-analytics:22.0.0")
     implementation("com.google.firebase:firebase-vertexai:16.0.0-beta01")
 
     testImplementation("junit:junit:4.13.2")
