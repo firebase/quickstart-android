20adcba46b2170bf662406b057eff8c55223aa79
DPEBot <dpebot@google.com>
2024-06-20 17:37:08 -0500

    Auto-update dependencies. (#1635)

--- crash/build.gradle.kts
+++ crash/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     id("com.android.library") version "8.5.0" apply false
     id("org.jetbrains.kotlin.android") version "2.0.0" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
-    id("com.google.firebase.crashlytics") version "3.0.1" apply false
+    id("com.google.firebase.crashlytics") version "3.0.2" apply false
 }
 
 allprojects {
