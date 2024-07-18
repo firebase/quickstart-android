2b704ba6fae041eced43f87c0029ce7170c77f5d
lehcar09 <rubies@google.com>
2024-07-18 21:57:13 +0800

    - Add Compose code for Analytics

---
+++ analytics/app/src/main/java/com/google/firebase/quickstart/analytics/kotlin/FirebaseAnalyticsViewModel.kt
@@ -0,0 +1,90 @@
+package com.google.firebase.quickstart.analytics.kotlin
+
+import android.content.Context
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.preference.PreferenceManager
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.analytics.ktx.logEvent
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.quickstart.analytics.kotlin.data.Constants
+
+class FirebaseAnalyticsViewModel (
+    private val firebaseAnalytics: FirebaseAnalytics
+): ViewModel() {
+
+    val showFavoriteFoodDialog = mutableStateOf(false)
+
+    private val _selectedImageIndex = mutableIntStateOf(0)
+    val selectedImageIndex: MutableState<Int> = _selectedImageIndex
+
+    private val _userFavoriteFood = mutableStateOf<String?>(null)
+    val userFavoriteFood: MutableState<String?> = _userFavoriteFood
+
+    fun setSelectedImageIndex(index: Int) {
+        _selectedImageIndex.intValue = index
+    }
+
+    fun setUserFavoriteFood(context: Context, food: String?) {
+        _userFavoriteFood.value = food
+        PreferenceManager.getDefaultSharedPreferences(context).edit()
+            .putString(Constants.KEY_FAVORITE_FOOD, food)
+            .apply()
+
+        // [START user_property]
+        firebaseAnalytics.setUserProperty("favorite_food", food)
+        // [END user_property]
+    }
+
+    fun getUserFavoriteFood(context: Context) {
+        _userFavoriteFood.value = PreferenceManager.getDefaultSharedPreferences(context)
+            .getString(Constants.KEY_FAVORITE_FOOD, null)
+    }
+
+    fun recordShare(imageTitle: String, text: String) {
+        firebaseAnalytics.logEvent("share_image") {
+            param("image_name", imageTitle)
+            param("full_text", text)
+        }
+    }
+
+    fun recordScreenView(screenName: String) {
+        // [START set_current_screen]
+        firebaseAnalytics.logEvent(FirebaseAnalytics.Event.SCREEN_VIEW) {
+            param(FirebaseAnalytics.Param.SCREEN_NAME, screenName)
+            param(FirebaseAnalytics.Param.SCREEN_CLASS, "MainActivity")
+        }
+        // [END set_current_screen]
+    }
+
+    fun recordImageView(id: String, name: String) {
+        // [START image_view_event]
+        firebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_ITEM) {
+            param(FirebaseAnalytics.Param.ITEM_ID, id)
+            param(FirebaseAnalytics.Param.ITEM_NAME, name)
+            param(FirebaseAnalytics.Param.CONTENT_TYPE, "image")
+        }
+        // [END image_view_event]
+    }
+
+    companion object {
+        const val TAG = "AnalyticsViewModel"
+        // Used to inject this ViewModel's dependencies
+        // See also: https://developer.android.com/topic/libraries/architecture/viewmodel/viewmodel-factories
+        val Factory: ViewModelProvider.Factory = object : ViewModelProvider.Factory {
+            @Suppress("UNCHECKED_CAST")
+            override fun <T : ViewModel> create(
+                modelClass: Class<T>,
+                extras: CreationExtras
+            ): T {
+                val firebaseAnalytics = Firebase.analytics
+                return FirebaseAnalyticsViewModel(firebaseAnalytics) as T
+            }
+        }
+    }
+}
