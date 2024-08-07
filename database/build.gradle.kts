381f233cc48d59f77785e41c5a30bd18ac7eb388
DPEBot <dpebot@google.com>
2024-08-07 07:25:56 -0500

    Auto-update dependencies. (#1649)

--- database/build.gradle.kts
+++ database/build.gradle.kts
@@ -3,7 +3,7 @@
 plugins {
     id("com.android.application") version "8.5.1" apply false
     id("com.android.library") version "8.5.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.10" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
 }
 
