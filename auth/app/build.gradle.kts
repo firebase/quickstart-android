efd7b70c7d9cc22354a3228a48fc907b624881d9
DPEBot <dpebot@google.com>
2024-09-19 08:08:46 -0400

    Auto-update dependencies. (#1668)

--- auth/app/build.gradle.kts
+++ auth/app/build.gradle.kts
@@ -55,8 +55,8 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.vectordrawable:vectordrawable-animated:1.2.0")
     implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.8.0")
-    implementation("androidx.navigation:navigation-ui-ktx:2.8.0")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.8.1")
+    implementation("androidx.navigation:navigation-ui-ktx:2.8.1")
 
     // Import the Firebase BoM (see: https://firebase.google.com/docs/android/learn-more#bom)
     implementation(platform("com.google.firebase:firebase-bom:33.3.0"))
--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     id("com.google.gms.google-services") version "4.4.2" apply false
     id("com.google.firebase.crashlytics") version "3.0.2" apply false
     id("com.google.firebase.firebase-perf") version "1.4.2" apply false
-    id("androidx.navigation.safeargs") version "2.8.0" apply false
+    id("androidx.navigation.safeargs") version "2.8.1" apply false
     id("com.github.ben-manes.versions") version "0.51.0" apply true
 }
 
