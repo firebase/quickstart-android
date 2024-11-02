077c6aab9ddf0fe2404e859cc0fcadcb3de70eb7
Rodrigo Lazo Paz <rlazo@google.com>
2024-11-01 20:13:40 -0400

    Remove kotlin(jvm) declaratino

--- vertexai/build.gradle.kts
+++ vertexai/build.gradle.kts
@@ -20,6 +20,5 @@ plugins {
     id("org.jetbrains.kotlin.android") version "2.0.21" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
-    kotlin("jvm") version "2.0.20"
     kotlin("plugin.serialization") version "2.0.20"
 }
