df6b5664822cfa8c5a393cb77b3c6b293660950c
DPEBot <dpebot@google.com>
2025-01-29 16:03:07 -0800

    Auto-update dependencies. (#2638)

--- admob/app/build.gradle.kts
+++ admob/app/build.gradle.kts
@@ -52,13 +52,13 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.browser:browser:1.5.0")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.8.5")
-    implementation("androidx.navigation:navigation-ui-ktx:2.8.5")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.8.6")
+    implementation("androidx.navigation:navigation-ui-ktx:2.8.6")
 
     implementation("com.google.android.gms:play-services-ads:23.3.0")
 
     // Import the Firebase BoM (see: https://firebase.google.com/docs/android/learn-more#bom)
-    implementation(platform("com.google.firebase:firebase-bom:33.7.0"))
+    implementation(platform("com.google.firebase:firebase-bom:33.8.0"))
 
     // For an optimal experience using AdMob, add the Firebase SDK
     // for Google Analytics. This is recommended, but not required.
--- perf/build.gradle.kts
+++ perf/build.gradle.kts
@@ -3,7 +3,7 @@
 plugins {
     id("com.android.application") version "8.8.0" apply false
     id("com.android.library") version "8.8.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.10" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
     id("com.google.firebase.firebase-perf") version "1.4.2" apply false
 }
