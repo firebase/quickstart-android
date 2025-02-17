ad17c02131147520a0059e0bc965ae94125ce3ea
DPEBot <dpebot@google.com>
2025-02-17 14:10:16 -0800

    Auto-update dependencies. (#2643)

--- crash/build.gradle.kts
+++ crash/build.gradle.kts
@@ -1,11 +1,11 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 plugins {
-    id("com.android.application") version "8.8.0" apply false
-    id("com.android.library") version "8.8.0" apply false
+    id("com.android.application") version "8.8.1" apply false
+    id("com.android.library") version "8.8.1" apply false
     id("org.jetbrains.kotlin.android") version "2.1.10" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
-    id("com.google.firebase.crashlytics") version "3.0.2" apply false
+    id("com.google.firebase.crashlytics") version "3.0.3" apply false
 }
 
 allprojects {
--- messaging/app/build.gradle.kts
+++ messaging/app/build.gradle.kts
@@ -59,12 +59,12 @@ dependencies {
 
     // Required when asking for permission to post notifications (starting in Android 13)
     implementation("androidx.activity:activity-ktx:1.10.0")
-    implementation("androidx.fragment:fragment-ktx:1.8.5")
+    implementation("androidx.fragment:fragment-ktx:1.8.6")
 
     implementation("com.google.android.material:material:1.12.0")
 
     // Import the Firebase BoM (see: https://firebase.google.com/docs/android/learn-more#bom)
-    implementation(platform("com.google.firebase:firebase-bom:33.8.0"))
+    implementation(platform("com.google.firebase:firebase-bom:33.9.0"))
 
     // Firebase Cloud Messaging
     implementation("com.google.firebase:firebase-messaging")
