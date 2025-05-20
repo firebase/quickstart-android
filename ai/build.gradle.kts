3bef700520a4f3e6ea4159a6e489c0cc9f684d39
rosariopf <rosariopf@google.com>
2025-05-20 17:51:28 +0100

    chore: apply compose plugin

--- ai/build.gradle.kts
+++ ai/build.gradle.kts
@@ -4,5 +4,6 @@ plugins {
     id("com.android.application") version "8.9.1" apply false
     id("com.android.library") version "8.9.1" apply false
     id("org.jetbrains.kotlin.android") version "2.1.20" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.1.20" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
 }
