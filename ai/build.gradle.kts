2932ba4502366683c91f38ea5fb8c052099bfc43
rosariopf <rosariopf@google.com>
2025-05-20 17:28:33 +0100

    Kotlin 2.1.20

--- ai/build.gradle.kts
+++ ai/build.gradle.kts
@@ -3,7 +3,7 @@
 plugins {
     id("com.android.application") version "8.9.1" apply false
     id("com.android.library") version "8.9.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.21" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.20" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
 }
 
