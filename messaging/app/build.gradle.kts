3afd4de8fce23807db172266cb4b6264a22b3413
DPEBot <dpebot@google.com>
2025-06-10 05:20:08 -0700

    Auto-update dependencies. (#2669)

--- messaging/app/build.gradle.kts
+++ messaging/app/build.gradle.kts
@@ -59,7 +59,7 @@ dependencies {
 
     // Required when asking for permission to post notifications (starting in Android 13)
     implementation("androidx.activity:activity-ktx:1.10.1")
-    implementation("androidx.fragment:fragment-ktx:1.8.6")
+    implementation("androidx.fragment:fragment-ktx:1.8.8")
 
     implementation("com.google.android.material:material:1.12.0")
 
@@ -75,7 +75,7 @@ dependencies {
 
     implementation("com.google.firebase:firebase-installations:18.0.0")
 
-    implementation("androidx.work:work-runtime:2.10.0")
+    implementation("androidx.work:work-runtime:2.10.1")
 
     // Testing dependencies
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
--- perf/app/build.gradle.kts
+++ perf/app/build.gradle.kts
@@ -68,7 +68,7 @@ dependencies {
 
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.1")
 
     implementation("com.github.bumptech.glide:glide:4.12.0")
 
