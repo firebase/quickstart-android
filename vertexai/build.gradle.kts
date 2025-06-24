7f3804a9d032166b918928cd56eb57d7da8b9f96
DPEBot <dpebot@google.com>
2025-06-24 07:03:47 -0700

    Auto-update dependencies. (#2681)

--- vertexai/build.gradle.kts
+++ vertexai/build.gradle.kts
@@ -17,8 +17,8 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.10.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.21" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.1.21" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.2.0" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
     kotlin("plugin.serialization") version "2.0.20"
 }
