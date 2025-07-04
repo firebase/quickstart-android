6a248d6ab37e5a5be886e29701001cd94215ac5a
rosariopf <rosariopf@google.com>
2025-07-04 16:29:33 +0100

    chore(ai): use kotlin 2.2.0

--- firebase-ai/app/build.gradle.kts
+++ firebase-ai/app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.compose")
-    kotlin("plugin.serialization") version "2.1.21"
+    id("org.jetbrains.kotlin.plugin.serialization")
     id("com.google.gms.google-services")
 }
 
--- firebase-ai/build.gradle.kts
+++ firebase-ai/build.gradle.kts
@@ -5,5 +5,6 @@ plugins {
     id("com.android.library") version "8.10.1" apply false
     id("org.jetbrains.kotlin.android") version "2.2.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.2.0" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.2.0" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
 }
