c3626e43ae9dc75e671c5c56eb062c26eee8db52
Rosário P. Fernandes <rosariopf@google.com>
2025-08-04 13:35:38 +0100

    refactor: use minSdk = 23 (#2703)

--- appdistribution/app/build.gradle.kts
+++ appdistribution/app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.google.firebase.appdistributionquickstart"
-        minSdk = 21
+        minSdk = 23
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
