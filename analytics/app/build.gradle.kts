4a85465565573b46d9efb5cf0a25928885040b5b
DPEBot <dpebot@google.com>
2025-08-13 06:29:52 -0700

    Auto-update dependencies. (#2711)

--- analytics/app/build.gradle.kts
+++ analytics/app/build.gradle.kts
@@ -59,7 +59,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.2")
 
     // Import the Firebase BoM (see: https://firebase.google.com/docs/android/learn-more#bom)
-    implementation(platform("com.google.firebase:firebase-bom:34.0.0"))
+    implementation(platform("com.google.firebase:firebase-bom:34.1.0"))
 
     // Firebase Analytics
     implementation("com.google.firebase:firebase-analytics")
