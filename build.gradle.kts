d20d2bfcb2b6cfbd792cbdfbd72f3f79269eff6e
Rosário P. Fernandes <rosariofernandes51@gmail.com>
2025-08-15 00:03:03 +0100

    chore: use AGP 9.0.0-alpha01

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 
 plugins {
-    id("com.android.application") version "8.12.0" apply false
-    id("com.android.library") version "8.12.0" apply false
+    id("com.android.application") version "9.0.0-alpha01" apply false
+    id("com.android.library") version "9.0.0-alpha01" apply false
     id("org.jetbrains.kotlin.android") version "2.2.0" apply false
     id("com.google.gms.google-services") version "4.4.3" apply false
     id("com.google.firebase.crashlytics") version "3.0.6" apply false
