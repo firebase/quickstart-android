69177a62dd24ae4fe52e4734ab185f0d3e84c0a7
Vinay Guthal <vguthal@google.com>
2025-08-26 13:55:28 -0400

    avoid merge conflicts

---
+++ firebase-ai/app/src/main/java/com/google/firebase/quickstart/ai/feature/media/imagen/EditingMode.kt
@@ -0,0 +1,9 @@
+package com.google.firebase.quickstart.ai.feature.media.imagen
+
+enum class EditingMode {
+    GENERATE,
+    INPAINTING,
+    OUTPAINTING,
+    SUBJECT_REFERENCE,
+    STYLE_TRANSFER,
+}
