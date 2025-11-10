43db7a156b9961f8a16a05128b5f23ad2873cc21
DPEBot <dpebot@google.com>
2025-11-10 12:05:59 -0800

    Auto-update dependencies. (#2728)

--- database/app/build.gradle.kts
+++ database/app/build.gradle.kts
@@ -54,8 +54,8 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("androidx.recyclerview:recyclerview:1.4.0")
     implementation("com.google.android.material:material:1.13.0")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.9.5")
-    implementation("androidx.navigation:navigation-ui-ktx:2.9.5")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.9.6")
+    implementation("androidx.navigation:navigation-ui-ktx:2.9.6")
 
     // Import the Firebase BoM (see: https://firebase.google.com/docs/android/learn-more#bom)
     implementation(platform("com.google.firebase:firebase-bom:34.5.0"))
