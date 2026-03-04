0da91fecd25797cae3823ee564277c84109c898d
Rosário P. Fernandes <rosariopf@google.com>
2026-03-04 20:33:04 +0000

    ci(dataconnect): use dataconnect:compile to generate SDKs in CI (#2766)
    This should update our CI pipeline to install the Firebase CLI and run `firebase dataconnect:compile` to generate the SDKs for the Data Connect quickstart

--- dataconnect/build.gradle.kts
+++ dataconnect/build.gradle.kts
@@ -4,3 +4,20 @@ plugins {
     alias(libs.plugins.google.services) apply false
     alias(libs.plugins.compose.compiler) apply false
 }
+
+tasks {
+    register<Exec>("dataconnectCompile") {
+        workingDir = project.file("./dataconnect")
+        if (org.apache.tools.ant.taskdefs.condition.Os.isFamily(org.apache.tools.ant.taskdefs.condition.Os.FAMILY_WINDOWS)) {
+            commandLine("npx.cmd", "-y", "firebase-tools@latest", "dataconnect:compile")
+        } else {
+            commandLine("npx", "-y", "firebase-tools@latest", "dataconnect:compile")
+        }
+        isIgnoreExitValue = true
+    }
+
+    register("clean", Delete::class) {
+        delete(rootProject.layout.buildDirectory)
+        finalizedBy("dataconnectCompile")
+    }
+}
