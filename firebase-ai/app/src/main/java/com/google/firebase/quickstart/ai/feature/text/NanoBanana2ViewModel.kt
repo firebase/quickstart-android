f18d873a7ee8795828cf6a4f8c1b348ff5cdeab9
Rosário P. Fernandes <rosariopf@google.com>
2026-05-28 18:55:37 +0100

    refactor(ai-logic): use nano banana GA models (#2797)

--- firebase-ai/app/src/main/java/com/google/firebase/quickstart/ai/feature/text/NanoBanana2ViewModel.kt
+++ firebase-ai/app/src/main/java/com/google/firebase/quickstart/ai/feature/text/NanoBanana2ViewModel.kt
@@ -5,27 +5,34 @@ import kotlinx.serialization.Serializable
 import com.google.firebase.Firebase
 import com.google.firebase.ai.Chat
 import com.google.firebase.ai.ai
+import com.google.firebase.ai.type.AspectRatio
 import com.google.firebase.ai.type.Content
 import com.google.firebase.ai.type.GenerativeBackend
 import com.google.firebase.ai.type.ResponseModality
 import com.google.firebase.ai.type.generationConfig
+import com.google.firebase.ai.type.imageConfig
 import com.google.firebase.quickstart.ai.ui.UiChatMessage
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 @Serializable
 object NanoBanana2Route
 
 class NanoBanana2ViewModel : ChatViewModel() {
-
-    override val initialPrompt: String = """
+    override val initialPrompt: String by lazy {
+        val currentMonthAndYear = SimpleDateFormat("MMM yyyy", Locale.US).format(Date())
+        """
         A photo of a glossy magazine cover, the minimal blue cover
         has the large bold words Nano Banana. The text is in a serif
         font and fills the view. No other text. In front of the text
         there is a portrait of a person in a sleek and minimal dress.
         She is playfully holding the number 2, which is the focal point.
-        Put the issue number and "Feb 2026" date in the corner along with
+        Put the issue number and "$currentMonthAndYear" date in the corner along with
         a barcode. The magazine is on a shelf against an orange plastered
         wall, within a designer store.
         """.trimIndent()
+    }
 
     private val chat: Chat
 
@@ -33,9 +40,13 @@ class NanoBanana2ViewModel : ChatViewModel() {
         val generativeModel = Firebase.ai(
             backend = GenerativeBackend.googleAI()
         ).generativeModel(
-            modelName = "gemini-3.1-flash-image-preview",
+            modelName = "gemini-3.1-flash-image",
             generationConfig = generationConfig {
                 responseModalities = listOf(ResponseModality.TEXT, ResponseModality.IMAGE)
+                // Optionally specify additional configuration
+                imageConfig = imageConfig {
+                    aspectRatio = AspectRatio.PORTRAIT_3x4
+                }
             }
         )
         chat = generativeModel.startChat()
